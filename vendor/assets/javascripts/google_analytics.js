f5aa119b5b71a78bd506e3073dd2d13a75d0eecf
apollo <super.hero2017@outlook.com>
2018-07-15 08:28:52 +0300

    Google Analytics

---
+++ vendor/assets/javascripts/google_analytics.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  
+  gtag('config', 'UA-104242001-1');
+});
