a358e356c28cd1baf1394bcf960bc3208203bf5a
apollo <super.hero2017@outlook.com>
2018-08-06 20:45:29 +0300

    Tracking code for entire site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,5 @@ Peatio::Application.configure do
   config.middleware.insert_before Rack::Runtime, Middleware::Security
 
   #Google analytics tracking code
-  GA.tracker = "UA-93356410-1"
+  GA.tracker = "UA-93308135-1"
 end
